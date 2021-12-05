3c0598cbe6aa616a09ef334b2b6bddf8c793f925
Vidit Khandelwal <63170797+csvidit@users.noreply.github.com>
2021-12-05 22:16:17 +0000

    small changes

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function menu()
 	{
 		$("#menu-items").removeClass("d-none");
 		$("#menu-items").addClass("d-flex");
+		$("#menu-items").addClass("animate__fadeInDown");
 	}
 	else
 	{
+		$("#menu-items").removeClass("animate__fadeInDown");
 		$("#menu-items").removeClass("d-flex");
 		$("#menu-items").addClass("d-none");
 	}
